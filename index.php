045ed4c5d8af85680d84fa9fda8f1ae26f2b9773
Ashraqat Nagdy <90476413+Ashraqat-Nagdy@users.noreply.github.com>
2021-12-05 20:57:14 +0200

    php
    1st assignment for NTI

---
+++ index.php
@@ -0,0 +1,17 @@
+
+  <p> Electricity Bill Calaulation </p>
+
+<?php
+ //calculate electrcity bill
+  $unit = 99 ;
+  // by using if statments
+
+  if ( $unit <= 50) {
+   echo ($unit = $unit * 3.50 ) ;
+  } elseif ($unit > 50 && $unit < 150){
+    echo ($unit = (($unit - 50 ) * 4 )+ 175);
+  } else{
+    echo ($unit = $unit * 6.50);
+  }
+
+?>
